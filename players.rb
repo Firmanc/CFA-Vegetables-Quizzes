6fee48068b6dfeb746681a126b055916aa96aa3a
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-02-21 15:14:54 +1100

    Upload player.rb

---
+++ players.rb
@@ -0,0 +1,23 @@
+class Players
+  def initialize
+    @name = name
+    @score = 0
+  end
+
+  attr_accessor :name, :score
+
+  #set up player name
+  def set_name
+    print "Please type in your name: "
+    @name = gets.chomp
+  end
+
+  #q_obj is the name of the questions object
+  def set_score(q_obj)
+    #increase score by 1 if answer was correct for last question
+    #by reading last value of answers array
+    if q_obj.answer[-1] == true
+      @score = @score + 1
+    end
+  end
+end
